e13d712c17b60c3f5d3913908730cb82d948b6f6
Igor <workuglis@gmail.com>
2020-04-04 17:49:00 +0300

    9. Написать тесты на StartUI.[#257549]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -3,61 +3,95 @@ package ru.job4j.tracker;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
-//    @Test
-//    public void whenAddItem() {
-//        String[] answers = {"Fix PC"};
-//        Input input = new StubInput(answers);
-//        Tracker tracker = new Tracker();
-//        UserAction[] actions = {new CreateAction()};
-//        StartUI startUI = new StartUI();
-//        startUI.init(input, tracker, actions);
-//        Item created = tracker.findAll()[0];
-//        Item expected = new Item("Fix PC");
-//        assertThat(created.getName(), is(expected.getName()));
-//    }
+    @Test
+    public void whenCreateItem() {
+        StubInput input = new StubInput(
+                new String[]{"0", "new item", "1"}
+        );
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        new StartUI().init(input, tracker, new UserAction[]{new CreateAction(), new ExitAction()});
+        Item expected = tracker.findAll()[0];
+        assertThat(item.getName(), is(expected.getName()));
+    }
 
-//    @Test
-//    public void whenCreateItem() {
-//        String[] answers = {"Igor", "Tanea", "Olea"};
-//        Input input = new StubInput(answers);
-//        Tracker tracker = new Tracker();
-//        StartUI.createItem(input, tracker);
-//        StartUI.createItem(input, tracker);
-//        StartUI.createItem(input, tracker);
-//        Item[] items = tracker.findAll();
-//        Item[] expected = {new Item("Igor"), new Item("Tanea"), new Item("Olea")};
-//        assertThat(items[0].getName(), is(expected[0].getName()));
-//        assertThat(items[1].getName(), is(expected[1].getName()));
-//        assertThat(items[2].getName(), is(expected[2].getName()));
-//    }
-//
-//    @Test
-//    public void whenReplaceItem() {
-//        Tracker tracker = new Tracker();
-//        Item item = new Item("Igor");
-//        tracker.add(item);
-//        String[] answer = {
-//                item.getId(), "Tanea"
-//        };
-//        StartUI.replaceItem(new StubInput(answer), tracker);
-//        Item replaced = tracker.findById(item.getId());
-//        assertThat(replaced.getName(), is("Tanea"));
-//    }
-//
-//    @Test
-//    public void whenDeleteItem() {
-//        Tracker tracker = new Tracker();
-//        Item item = new Item("Igor");
-//        tracker.add(item);
-//        String[] answer = {
-//                item.getId()
-//        };
-//        StartUI.deleteItem(new StubInput(answer), tracker);
-//        Item deleted = tracker.findById(item.getId());
-//        assertNull(deleted);
-//    }
+    @Test
+    public void whenFindByName() {
+        StubInput input = new StubInput(
+                new String[]{"0", "new item",
+                        "0", "new item2",
+                        "0", "new item3",
+                        "0", "new item",
+                        "1", "new item",
+                        "2"
+                }
+        );
+        Tracker tracker = new Tracker();
+        Item[] items = new Item[]{
+                new Item("new item"),
+                new Item("new item")
+        };
+        new StartUI().init(input, tracker,
+                new UserAction[]{
+                        new CreateAction(),
+                        new FindByNameAction(),
+                        new ExitAction()});
+        Item[] expected = tracker.findByName("new item");
+        assertThat(items[0].getName(), is(expected[0].getName()));
+        assertThat(items[1].getName(), is(expected[1].getName()));
+    }
+
+    @Test
+    public void whenShowAllItems() {
+        StubInput input = new StubInput(
+                new String[]{"0", "new item",
+                        "0", "new item2",
+                        "0", "new item3",
+                        "0", "new item",
+                        "1",
+                        "2"
+                }
+        );
+        Tracker tracker = new Tracker();
+        Item[] items = new Item[]{
+                new Item("new item"),
+                new Item("new item2"),
+                new Item("new item3"),
+                new Item("new item")
+        };
+        new StartUI().init(input, tracker,
+                new UserAction[]{
+                        new CreateAction(),
+                        new ShowAllItemsAction(),
+                        new ExitAction()});
+        Item[] expected = tracker.findAll();
+        assertThat(items.length, is(expected.length));
+    }
+
+    @Test
+    public void whenFindById() {
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("need item"));
+        String id = tracker.findAll()[0].getId();
+        StubInput input = new StubInput(
+                new String[]{"0", "new item",
+                        "0", "new item2",
+                        "0", "new item3",
+                        "0", "new item",
+                        "1", id,
+                        "2"
+                }
+        );
+        new StartUI().init(input, tracker,
+                new UserAction[]{
+                        new CreateAction(),
+                        new FindByIdAction(),
+                        new ExitAction()});
+        Item need = tracker.findAll()[0];
+        Item expected = new Item("need item");
+        assertThat(need.getName(), is(expected.getName()));
+    }
 }
